632375176c23f427cdc811529357192bfd7fa5d7
Anthony Parrino <anthony21parrino21@gmail.com>
2022-03-29 16:50:38 -0400

    added pixel

--- client/src/pages/ThankYouPages/ThankYouLife/ThankYouLife.js
+++ client/src/pages/ThankYouPages/ThankYouLife/ThankYouLife.js
@@ -10,6 +10,9 @@ const ThankYouLife = () => {
                 <h1 className='thank-you'>
                     Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
                 </h1>
+                {/* Offer Conversion: MyRateChamp.com - Life Insurance - US */}
+                <iframe title='pixel' src="https://offers.axtracking.com/aff_l?offer_id=1277" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+                {/* End Offer Conversion */}
             </div>
         </div>
     )
