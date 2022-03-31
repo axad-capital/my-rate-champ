8459b605d2bf3e55ca4ed9fa6eeb3523023a128c
Anthony Parrino <anthony21parrino21@gmail.com>
2022-03-31 10:38:38 -0400

    added ads for all pages

--- client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
+++ client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
@@ -1,11 +1,15 @@
 import React from 'react'
 import './thankYouAuto.css'
-import NavHome from '../../../components/NavComp/NavHome/NavHome'
+// import NavHome from '../../../components/NavComp/NavHome/NavHome'
 
 const ThankYouAuto = () => {
+    function goBack() {
+        localStorage.clear()
+        window.location.href = '/'
+    }
     return (
         <div>
-            <NavHome />
+            <button className='go-back' onClick={goBack}>Done</button>
             <div>
                 <h1 className='thank-you'>
                     Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
--- client/src/pages/ThankYouPages/ThankYouHealth/ThankYouHealth.js
+++ client/src/pages/ThankYouPages/ThankYouHealth/ThankYouHealth.js
@@ -1,10 +1,14 @@
 import React from 'react'
-import NavHome from '../../../components/NavComp/NavHome/NavHome'
+// import NavHome from '../../../components/NavComp/NavHome/NavHome'
 
 const ThankYouHealth = () => {
+    function goBack() {
+        localStorage.clear()
+        window.location.href = '/'
+    }
     return (
         <div>
-            <NavHome />
+            <button className='go-back' onClick={goBack}>Done</button>
             <div>
                 <h1 className='thank-you'>
                     Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
