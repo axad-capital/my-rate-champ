20e0b9ca6f4863e1ed987ce38953882a7d014366
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-21 16:12:24 -0400

    added new pixels

--- client/src/pages/ThankYouPages/ThankYouHealth/ThankYouHealth.js
+++ client/src/pages/ThankYouPages/ThankYouHealth/ThankYouHealth.js
@@ -14,7 +14,7 @@ const ThankYouHealth = () => {
                     Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
                 </h1>
                 {/* Offer Conversion: MyRateChamp.com - Health Insurance - US */}
-                <iframe title='health-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1280" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+                <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1280&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
                 {/* End Offer Conversion */}
             </div>
         </div>
