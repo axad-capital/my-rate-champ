40d0e205c5283e572311434ad25e0e7c71f9c853
Anthony Parrino <anthony21parrino21@gmail.com>
2022-04-22 12:54:15 -0400

    added old pixel

--- client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
+++ client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
@@ -15,6 +15,7 @@ const ThankYouAuto = () => {
                     Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
                 </h1>
                 {/* Offer Conversion: MyRateChamp.com - Auto Insurance - US */}
+                <iframe title='auto-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1279" scrolling="no" frameborder="0" width="1" height="1"></iframe>
                 <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1279&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
                 {/* End Offer Conversion */}
             </div>
