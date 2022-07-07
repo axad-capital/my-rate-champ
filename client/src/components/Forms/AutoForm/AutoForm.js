a6b436d7d6e2a45ca142761d71e94ac1cb05e5f0
Anthony Parrino <anthonyparrino90@gmail.com>
2022-07-07 13:38:58 -0400

    added transparently ads to auto

--- client/src/components/Forms/AutoForm/AutoForm.js
+++ client/src/components/Forms/AutoForm/AutoForm.js
@@ -1,81 +1,49 @@
-import React, {useState} from 'react';
+import React, { useState } from 'react';
 import './form.css';
-// import { v4 as uuidv4 } from 'uuid'
 
 const Form = () => {
 
-    // function quoteFormHandler() {
-
-    //     fetch('https://api.ipify.org/?format=json')
-    //         .then(result => result.json())
-    //         .then(data => {
-
-    //             let autoData = {
-    //                 id: uuidv4(),
-    //                 firstName: document.getElementById('first').value,
-    //                 lastName: document.getElementById('last').value,
-    //                 birth: document.getElementById('bday').value,
-    //                 age: document.getElementById('age').value,
-    //                 email: document.getElementById('email').value,
-    //                 homeOwner: document.getElementById('home-owner').value,
-    //                 insured: document.getElementById('insured').value,
-    //                 vehicles: document.getElementById('vehicles').value,
-    //                 phone: document.getElementById('phone-num').value,
-    //                 zipcode: document.getElementById('zip').value
-    //             }
-
-    //             fetch('http://datapass.quinstage.com/calls/auto/callsping?quadTag={quad_tag}', {
-    //                 method: 'POST',
-    //                 headers: {
-    //                     'Content-Type': 'application/json'
-    //                 },
-    //                 body: {
-    //                     'tracking': {
-    //                         'content_type': 'xml/json',
-    //                         'ni_ad_client': '',
-    //                         'ni_zc': autoData.zip,
-    //                         'ip': data.ip,
-    //                         'ua': navigator.userAgent,
-    //                         'ni_var1': autoData.id,
-    //                         'ni_ref': '/auto-thank-you'
-    //                     },
-    //                     'contact': {
-    //                         'first_name': autoData.firstName,
-    //                         'last_name': autoData.lastName,
-    //                         'email': autoData.email,
-    //                         'zip': autoData.zip,
-    //                         'homeowner': autoData.homeOwner,
-    //                     },
-    //                     'current_insurance': {
-    //                         'currently_insured': autoData.insured,
-    //                     },
-    //                     'requested_coverage': {
-    //                         'vehicle_count': autoData.vehicles,
-    //                     },
-    //                     'individuals': {
-    //                         'birthdate': autoData.birthdate,
-    //                         'age': autoData.age
-    //                     }
-    //                 }
-    //             })
-
-    //         })
-
-
-
-    //     // window.location.href = '/thank-you-auto'
-    // }
-
-    const  [blankHandler, setBlankHandler] = useState('')
+    const [blankHandler, setBlankHandler] = useState('')
 
     function quoteFormHandler() {
-        if (document.getElementById('auto-zip').value === null || document.getElementById('auto-zip').value === '' || document.getElementById('auto-zip').value.length < 5) {
-            setBlankHandler('You Must Enter A Valid Zipcode!')
+        // if (document.getElementById('auto-zip').value === null || document.getElementById('auto-zip').value === '' || document.getElementById('auto-zip').value.length < 5) {
+        //     setBlankHandler('You Must Enter A Valid Zipcode!')
+        //     return
+        // }
+        // let autoZip = document.getElementById('auto-zip').value
+        // localStorage.setItem('zipcodeAuto', autoZip)
+        // window.location.href = '/thank-you-auto'
+        if (document.getElementById('state').value === '' || document.getElementById('home-owner').value === '' || document.getElementById('insured').value === '' || document.getElementById('age').value === '') {
+            setBlankHandler('All fields required')
             return
         }
-        let autoZip = document.getElementById('auto-zip').value
-        localStorage.setItem('zipcodeAuto', autoZip)
-        window.location.href = '/thank-you-auto'
+
+        if (parseInt(document.getElementById('age').value) <= 0) {
+            setBlankHandler('Please enter a valid age')
+            return
+        }
+
+        let userInput = {
+            "state": document.getElementById('state').value,
+            "pubcampaignid": "6364",
+            "vertical": "2",
+            "age": document.getElementById('age').value,
+            "homeowner": document.getElementById('home-owner').value,
+            "currentlyinsured": document.getElementById('insured').value
+        }
+
+        fetch('https://api.transparent.ly/search/blue/green', {
+            method: 'POST',
+            headers: {
+                'Content-Type': 'application/json'
+            },
+            body: JSON.stringify(userInput)
+        }).then(res => res.json())
+            .then(data => {
+                console.log(data)
+                localStorage.setItem('zipcodeAuto', JSON.stringify(data))
+                window.location.href = '/thank-you-auto'
+            })
     }
 
     return (
@@ -85,46 +53,90 @@ const Form = () => {
                 <p>Stay Home & Find Affordable Car Insurance In 2 Minutes</p>
                 <div className='auto-form'>
 
-                    {/* <label htmlFor="first">First Name</label>
-                    <input id='first' name='first' className='form-input' type="text" placeholder='First Name' />
-
-                    <label htmlFor="last">Last Name</label>
-                    <input id='last' name='last' className='form-input' type="text" placeholder='Last Name' /> */}
-
                     <label className='zip-code-label' htmlFor="zip">Enter Your Zipcode</label>
                     <input id='auto-zip' name='zip' className='form-input' type="text" placeholder='Zipcode' />
+                    <br />
 
-                    {/* <label htmlFor="email">Email</label>
-                    <input id='email' name='email' className='form-input' type="text" placeholder='Email' />
+                    <label htmlFor="state"><strong>Please choose the current state you're in</strong></label>
+                    <select style={{ padding: '10px' }} id='state' className='form-input' name="state">
+                        <option disabled selected value=''> -- Select An Option -- </option>
+                        <option value="AL">Alabama</option>
+                        <option value="AK">Alaska</option>
+                        <option value="AZ">Arizona</option>
+                        <option value="AR">Arkansas</option>
+                        <option value="CA">California</option>
+                        <option value="CO">Colorado</option>
+                        <option value="CT">Connecticut</option>
+                        <option value="DE">Delaware</option>
+                        <option value="DC">District Of Columbia</option>
+                        <option value="FL">Florida</option>
+                        <option value="GA">Georgia</option>
+                        <option value="HI">Hawaii</option>
+                        <option value="ID">Idaho</option>
+                        <option value="IL">Illinois</option>
+                        <option value="IN">Indiana</option>
+                        <option value="IA">Iowa</option>
+                        <option value="KS">Kansas</option>
+                        <option value="KY">Kentucky</option>
+                        <option value="LA">Louisiana</option>
+                        <option value="ME">Maine</option>
+                        <option value="MD">Maryland</option>
+                        <option value="MA">Massachusetts</option>
+                        <option value="MI">Michigan</option>
+                        <option value="MN">Minnesota</option>
+                        <option value="MS">Mississippi</option>
+                        <option value="MO">Missouri</option>
+                        <option value="MT">Montana</option>
+                        <option value="NE">Nebraska</option>
+                        <option value="NV">Nevada</option>
+                        <option value="NH">New Hampshire</option>
+                        <option value="NJ">New Jersey</option>
+                        <option value="NM">New Mexico</option>
+                        <option value="NY">New York</option>
+                        <option value="NC">North Carolina</option>
+                        <option value="ND">North Dakota</option>
+                        <option value="OH">Ohio</option>
+                        <option value="OK">Oklahoma</option>
+                        <option value="OR">Oregon</option>
+                        <option value="PA">Pennsylvania</option>
+                        <option value="RI">Rhode Island</option>
+                        <option value="SC">South Carolina</option>
+                        <option value="SD">South Dakota</option>
+                        <option value="TN">Tennessee</option>
+                        <option value="TX">Texas</option>
+                        <option value="UT">Utah</option>
+                        <option value="VT">Vermont</option>
+                        <option value="VA">Virginia</option>
+                        <option value="WA">Washington</option>
+                        <option value="WV">West Virginia</option>
+                        <option value="WI">Wisconsin</option>
+                        <option value="WY">Wyoming</option>
+                    </select>
+                    <br />
 
-                    <label htmlFor="bday">Date of Birth</label>
-                    <input id='bday' name='bday' className='form-input' type="date" /> */}
-
-                    {/* <label htmlFor="vehicles">Current Age</label>
-                    <input id='age' name='age' className='form-input' type="number" placeholder='Age' /> */}
-
-                    {/* <label htmlFor="home-owner">Are you a Home Owner?</label>
-                    <select id='home-owner' className='form-input' name="home-owner">
+                    <label htmlFor="home-owner"><strong>Are you a homeowner?</strong></label>
+                    <select style={{ padding: '10px' }} id='home-owner' className='form-input' name="home-owner">
+                        <option disabled selected value=''> -- Select An Option -- </option>
                         <option value="1">Yes</option>
                         <option value="0">No</option>
                     </select>
-
-                    <label htmlFor="insured">Are you currently insured?</label>
-                    <select id='insured' className='form-input' name="insured">
-                        <option value="Yes">Yes</option>
-                        <option value="No">No</option>
-                    </select> */}
-
-                    {/* <label htmlFor="vehicles">How Many Vehicles Do You Own</label>
-                    <input id='vehicles' name='vehicles' className='form-input' type="number" placeholder='Vehicle Count' /> */}
-
-                    {/* <label htmlFor="phone-num">Phone Number</label>
-                    <input id='phone-num' name='phone-num' className='form-input' type="text" placeholder='Phone Number' /> */}
-
                     <br />
+
+                    <label htmlFor="insured"><strong>Are you a insured?</strong></label>
+                    <select style={{ padding: '10px' }} id='insured' className='form-input' name="insured">
+                        <option disabled selected value=''> -- Select An Option -- </option>
+                        <option value="1">Yes</option>
+                        <option value="0">No</option>
+                    </select>
+                    <br />
+
+                    <label htmlFor="age"><strong>Enter your age</strong></label>
+                    <input style={{ padding: '10px' }} id='age' className='form-input' type="number" placeholder='Enter your age' />
+                    <br />
+
                     <button className='form-btn disable' onClick={quoteFormHandler}>Submit</button>
                     <br />
-                    <p style={{color: 'red', textAlign: 'center'}} >{blankHandler}</p>
+                    <p style={{ color: 'red', textAlign: 'center' }} >{blankHandler}</p>
                 </div>
             </div>
         </div>
--- client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
+++ client/src/pages/ThankYouPages/ThankYouAuto/ThankYouAuto.js
@@ -7,20 +7,52 @@ const ThankYouAuto = () => {
         localStorage.clear()
         window.location.href = '/'
     }
-    return (
-        <div>
-            <button className='go-back' onClick={goBack}>Done</button>
+    // return (
+    //     <div>
+    //         <button className='go-back' onClick={goBack}>Done</button>
+    //         <div>
+    //             <h1 className='thank-you'>
+    //                 Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
+    //             </h1>
+    //             {/* Offer Conversion: MyRateChamp.com - Auto Insurance - US */}
+    //             <iframe title='auto-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1279" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+    //             <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1279&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+    //             {/* End Offer Conversion */}
+    //         </div>
+    //     </div>
+    // )
+    let storage = JSON.parse(localStorage.getItem('zipcodeAuto'))
+    console.log(storage.result)
+
+    if (storage === null) {
+        return (
             <div>
-                <h1 className='thank-you'>
-                    Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.
-                </h1>
-                {/* Offer Conversion: MyRateChamp.com - Auto Insurance - US */}
-                <iframe title='auto-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1279" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+                <button className='go-back' onClick={goBack}>Done</button>
+                <h1>No ads to show at this time</h1>
                 <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1279&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
-                {/* End Offer Conversion */}
+                <iframe title='auto-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1279" scrolling="no" frameborder="0" width="1" height="1"></iframe>
             </div>
-        </div>
-    )
+        )
+    } else {
+        return (
+            <div>
+                <button className='go-back' onClick={goBack}>Done</button>
+                <h1 style={{ width: '80%', margin: '20px auto', textAlign: 'center' }}>Compare Insurance Quotes in Your Area. Click on 2 or more Insurance Partners below to compare quotes and maximize your savings.</h1>
+                <br />
+                {storage.result.map(ad => (
+                    <div className='transparently' onClick={() => window.location.href = ad.clickUrl} key={ad.adCreativeId}>
+                        <img style={{ width: '200px' }} src={ad.logoUrl} alt="ad logo" />
+                        <div>
+                            <h2>{ad.brandName}</h2>
+                            <p>{ad.title}</p>
+                        </div>
+                        <iframe title='pixel' src="https://www.a1jkghtrk.com/?nid=1715&oid=1279&amount=AMOUNT" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+                        <iframe title='auto-pixel' src="https://offers.axtracking.com/aff_l?offer_id=1279" scrolling="no" frameborder="0" width="1" height="1"></iframe>
+                    </div>
+                ))}
+            </div>
+        )
+    }
 }
 
 export default ThankYouAuto
